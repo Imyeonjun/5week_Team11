8192f6116083c2daf82515fb4e4ee120d319eabe
Imyeonjun <imyeonjun0602@gmail.com>
2025-05-08 15:07:53 +0900

    Update MapCreator.cs
    - 프리팹을 가져와 맵 구성하는 로직.

--- Assets/Scripts/Map/MapCreator.cs
+++ Assets/Scripts/Map/MapCreator.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class MapCreator : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject[] roomTemplates; // 템플릿 프리팹 불러오기
+
+    [Header("스테이지 생성 범위")]
+    public int stageWidth = 5;
+    public int stageHeight = 5;
+
+    [Header("스테이지 간격")]
+    public float roomSpacing = 10f;
+
     void Start()
     {
-        
+        CreatStage();
     }
 
-    // Update is called once per frame
-    void Update()
+    void CreatStage()
     {
-        
+        for (int x = 0; x < stageWidth; x++)
+        {
+            for (int y = 0; y < stageHeight; y++)
+            {
+                Vector2 pos = new Vector2(x * roomSpacing, y * roomSpacing);
+                int index = Random.Range(0, roomTemplates.Length);
+                Instantiate(roomTemplates[index], pos, Quaternion.identity);
+            }
+        }
     }
 }
