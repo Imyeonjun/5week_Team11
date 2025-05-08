098bfba98650f867869a51ab66db96e072f35cfb
ParkMinsoo <harimaho88@gmail.com>
2025-05-08 14:48:01 +0900

    playercontroller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+
+    [SerializeField] private SpriteRenderer characterRenderer;
+    [SerializeField] private Transform weaponPivot;
+    
+    private Vector2 movementDirection = Vector2.zero;
+    private Vector2 lookDirection = Vector2.zero;
+    private Vector2 knockback = Vector2.zero;
+    private float knockbackDuration = 0.0f;
+
+    private Camera camera;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+    private void Start()
+    {
+        camera = Camera.main;
+    }
+    private void Update()
+    {
+        HandleAction();
+        Rotate(lookDirection);
+    }
+
+    private void FixedUpdate()
+    {
+        Movement(movementDirection);
+        if(knockbackDuration > 0.0f)
+        {
+            knockbackDuration -= Time.fixedDeltaTime;
+        }
+    }
+    private void HandleAction()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        movementDirection = new Vector2(horizontal, vertical).normalized;
+
+        Vector2 mousePosition = Input.mousePosition;
+        Vector2 worldPos = camera.ScreenToWorldPoint(mousePosition);
+        lookDirection = (worldPos - (Vector2)transform.position);
+
+        if (lookDirection.magnitude < .9f)
+        {
+            lookDirection = Vector2.zero;
+        }
+        else
+        {
+            lookDirection = lookDirection.normalized;
+        }
+    }
+    
+    // 무기 회전에 필요할 듯
+    private void Rotate(Vector2 direction)
+    {
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        bool isLeft = Mathf.Abs(rotZ) > 90f;
+
+        characterRenderer.flipX = isLeft;
+
+        if (weaponPivot != null)
+        {
+            weaponPivot.rotation = Quaternion.Euler(0, 0, rotZ);
+        }
+    }
+
+    private void Movement(Vector2 direction)
+    {
+        direction = direction * 5;
+        if (knockbackDuration > 0.0f)
+        {
+            direction *= 0.2f;
+            direction += knockback;
+        }
+
+        _rigidbody.velocity = direction;
+    }
+
+    public void ApplyKnockback(Transform other, float power, float duration)
+    {
+        knockbackDuration = duration;
+        knockback = -(other.position - transform.position).normalized * power;
+    }
+
+}
