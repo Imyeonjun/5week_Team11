51a3f19a1d4189e728b27b7890da9c04f6c5013c
Imyeonjun <imyeonjun0602@gmail.com>
2025-05-09 10:55:17 +0900

    Creat RoomTemplate.cs

--- Assets/Scripts/Map/MapCreator.cs
+++ Assets/Scripts/Map/MapCreator.cs
@@ -20,14 +20,60 @@ public class MapCreator : MonoBehaviour
 
     void CreatStage()
     {
+        GameObject[,] map = new GameObject[stageWidth, stageHeight];
+
         for (int x = 0; x < stageWidth; x++)
         {
             for (int y = 0; y < stageHeight; y++)
             {
                 Vector2 pos = new Vector2(x * roomSpacing, y * roomSpacing);
-                int index = Random.Range(0, roomTemplates.Length);
-                Instantiate(roomTemplates[index], pos, Quaternion.identity);
+                GameObject chosen = null;
+
+                // 이전 방 정보 가져오기 (왼쪽 또는 아래쪽)
+                RoomTemplate prevRoomLeft = null;
+                RoomTemplate prevRoomDown = null;
+
+                if (x > 0 && map[x - 1, y] != null)
+                    prevRoomLeft = map[x - 1, y].GetComponent<RoomTemplate>();
+
+                if (y > 0 && map[x, y - 1] != null)
+                    prevRoomDown = map[x, y - 1].GetComponent<RoomTemplate>();
+
+                // 프리팹 중 조건 맞는 거 찾기
+                foreach (GameObject prefab in roomTemplates)
+                {
+                    RoomTemplate candidate = prefab.GetComponent<RoomTemplate>();
+                    bool fits = true;
+
+                    if (prevRoomLeft != null)
+                    {
+                        if (!(prevRoomLeft.openRight && candidate.openLeft))
+                            fits = false;
+                    }
+
+                    if (prevRoomDown != null)
+                    {
+                        if (!(prevRoomDown.openTop && candidate.openBottom))
+                            fits = false;
+                    }
+
+                    if (fits)
+                    {
+                        chosen = prefab;
+                        break;
+                    }
+                }
+
+                // 조건에 맞는 프리팹이 없으면 아무거나 배치하거나 빈 방 처리
+                if (chosen == null)
+                {
+                    int randomIndex = Random.Range(0, roomTemplates.Length);
+                    chosen = roomTemplates[randomIndex];
+                }
+
+                map[x, y] = Instantiate(chosen, pos, Quaternion.identity);
             }
         }
     }
+
 }
---
+++ Assets/Scripts/Map/RoomTemplate.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class RoomTemplate : MonoBehaviour
+{
+    public bool openTop;
+    public bool openBottom;
+    public bool openLeft;
+    public bool openRight;
+}
