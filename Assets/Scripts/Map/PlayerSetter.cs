a2aaee16fa664139ce0d2f283276ccc3792b5447
ParkMinsoo <harimaho88@gmail.com>
2025-05-13 02:11:55 +0900

    플레이어 웨폰피벗 수정

--- Assets/Scripts/Map/PlayerSetter.cs
+++ Assets/Scripts/Map/PlayerSetter.cs
@@ -6,7 +6,7 @@ public class PlayerSetter : MonoBehaviour
 {
     [SerializeField] private Transform player; // 이미 존재하는 플레이어
     [SerializeField] private MapCreator mapCreator; // MapCreator 참조
-    public FollowCamera followCamera;
+    
 
     void Start()
     {
