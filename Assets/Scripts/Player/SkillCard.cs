69375f57247853bfc3fc0684d2f606cb0d1fbb74
Saint_Elion <pocklyuck@naver.com>
2025-05-12 19:52:48 +0900

    Player 수정
    feat: 스킬 프리팹 구성 중

---
+++ Assets/Scripts/Player/SkillCard.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillCard : MonoBehaviour
+{
+    private GameObject skillCardBg;
+    void Start()
+    {
+        skillCardBg = GetComponentInChildren<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
