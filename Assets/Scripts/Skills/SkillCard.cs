ef4205190359877838b4ac15c36bc247cec5f258
Saint_Elion <pocklyuck@naver.com>
2025-05-12 20:13:03 +0900

    Player 수정
    sytle: 스킬카드 스크립트 두 개로 분리

--- Assets/Scripts/Skills/SkillCard.cs
+++ Assets/Scripts/Skills/SkillCard.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SkillCard : MonoBehaviour
+public class SkillCards : MonoBehaviour
 {
-    private GameObject skillCardBg;
+    // Start is called before the first frame update
     void Start()
     {
-        skillCardBg = GetComponentInChildren<GameObject>();
+        
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/Skills/SkillCardShower.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillCard : MonoBehaviour
+{
+    private GameObject skillCardBg;
+    void Start()
+    {
+        skillCardBg = GetComponentInChildren<GameObject>();
+    }
+
+    void SkillCards()
+    {
+
+    }
+}
