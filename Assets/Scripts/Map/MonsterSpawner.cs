6d9fc113fd4fadbd50e72a9c1bc23f90b1399a45
Imyeonjun <imyeonjun0602@gmail.com>
2025-05-13 11:50:52 +0900

    Update MonsterSpawner.cs

--- Assets/Scripts/Map/MonsterSpawner.cs
+++ Assets/Scripts/Map/MonsterSpawner.cs
@@ -5,8 +5,9 @@ using UnityEngine.Tilemaps;
 
 public class MonsterSpawner : MonoBehaviour
 {
-    [SerializeField] private MapCreator mapCreator; // 맵 생성기 참조
-    [SerializeField] private GameObject monsterPrefab; // 몬스터 프리팹
+    [SerializeField] private Transform player;
+    [SerializeField] private MapCreator mapCreator;
+    [SerializeField] private GameObject monsterPrefab;
 
     void Start()
     {
@@ -15,12 +16,19 @@ public class MonsterSpawner : MonoBehaviour
 
     IEnumerator SpawnMonstersWhenReady()
     {
-        // 맵 생성 완료될 때까지 대기
         while (!mapCreator.IsGenerationComplete)
             yield return null;
 
+        // 플레이어가 위치한 방 좌표 계산
+        Vector2Int playerCell = new Vector2Int(
+            Mathf.RoundToInt(player.position.x / mapCreator.roomSpacing),
+            Mathf.RoundToInt(player.position.y / mapCreator.roomSpacing)
+        );
+
         foreach (Vector2Int pos in mapCreator.RoomPositions)
         {
+            if (pos == playerCell) continue; // 플레이어가 있는 방은 제외
+
             GameObject room = mapCreator.GetRoomAt(pos);
             if (room == null) continue;
 
