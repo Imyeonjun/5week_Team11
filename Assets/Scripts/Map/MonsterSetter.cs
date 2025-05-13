c67d6131fc79482b8f0dc8646bb05e5c14a84f31
Imyeonjun <imyeonjun0602@gmail.com>
2025-05-13 15:39:31 +0900

    style 몬스터스포너 -> 몬스터세터

--- Assets/Scripts/Map/MonsterSetter.cs
+++ Assets/Scripts/Map/MonsterSetter.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class MonsterSpawner : MonoBehaviour
+public class MonsterSetter : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private MapCreator mapCreator;
